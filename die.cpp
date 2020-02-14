5150c53f3b2b0ec6ea3919077b52e31575917879
Eric <eric.s.chapman10@gmail.com>
2020-02-14 13:46:28 -0500

    End of class

--- die.cpp
+++ die.cpp
@@ -3,3 +3,56 @@
 //
 
 #include "die.h"
+#include <random>
+
+Die (int startingNumberOfSides, int startingIncrements=1, int firstStartingVal=1, std::string startingColor = "white", bool startingIsSymbol=false, std::string startingMaterial="Bone", int startingValue=1);
+
+
+int Die::roll(){
+    value = 2;
+}
+
+void Die::setNumberOfSides(int newNumberOfSides){
+    numberOfSides = newNumberofSides;
+}
+
+int Die::getNumberOfSides(){
+
+}
+
+void Die::setIncrement(int newIncrement){
+
+}
+int Die::getIncrement(){
+
+}
+void Die::setStartingValue(int newStartingSide){
+
+}
+int Die::getStartingValue(){
+
+}
+void Die::setValue(int newValue){
+
+}
+int Die::getValue(){
+
+}
+void Die::setColor(std::string newColor){
+
+}
+std::string Die::getColor(){
+
+}
+void Die::setIsSymbol(bool newIsSymbol){
+
+}
+bool Die::isSymbol(){
+
+}
+void Die::setMaterial(std::string newMaterial){
+
+}
+std::string Die::getMaterial(){
+
+}
--- die.h
+++ die.h
@@ -1,8 +1,38 @@
 //
 // Created by erics on 2/14/2020.
 //
-
+#include <iostream>
 #ifndef CRAPS_DIE_H
 #define CRAPS_DIE_H
 
+class Die {
+private:
+    int numberOfSides;
+    int increment;
+    int startingVal;
+    std::string color;
+    bool isSymbols;
+    std::string material;
+    int value;
+public:
+    Die (int startingNumberOfSides, int startingIncrements=1, int firstStartingVal=1, std::string startingColor = "white", bool startingIsSymbol=false, std::string startingMaterial="Bone", int startingValue=1);
+
+    int roll();
+
+    void setNumberOfSides(int newNumberOfSides);
+    int getNumberOfSides();
+    void setIncrement(int newIncrement);
+    int getIncrement();
+    void setStartingValue(int newStartingSide);
+    int getStartingValue();
+    void setValue(int newValue);
+    int getValue();
+    void setColor(std::string newColor);
+    std::string getColor();
+    void setIsSymbol(bool newIsSymbol);
+    bool isSymbol();
+    void setMaterial(std::string newMaterial);
+    std::string getMaterial();
+
+};
 #endif //CRAPS_DIE_H
