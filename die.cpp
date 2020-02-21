9d16940eab4e733355e4b7961700e33d0ccb40fe
Eric <eric.s.chapman10@gmail.com>
2020-02-21 11:31:31 -0500

    Filled out all of the setters and getters that we started in class.

--- die.cpp
+++ die.cpp
@@ -5,7 +5,7 @@
 #include "die.h"
 #include <random>
 
-Die (int startingNumberOfSides, int startingIncrements=1, int firstStartingVal=1, std::string startingColor = "white", bool startingIsSymbol=false, std::string startingMaterial="Bone", int startingValue=1);
+Die (int startingNumberOfSides = 6, int startingIncrements=1, int firstStartingVal=1, std::string startingColor = "white", bool startingIsSymbol=false, std::string startingMaterial="Bone", int startingValue=1);
 
 
 int Die::roll(){
@@ -13,46 +13,46 @@ int Die::roll(){
 }
 
 void Die::setNumberOfSides(int newNumberOfSides){
-    numberOfSides = newNumberofSides;
+    numberOfSides = newNumberOfSides;
 }
 
 int Die::getNumberOfSides(){
-
+    return numberOfSides;
 }
 
 void Die::setIncrement(int newIncrement){
-
+    increment = newIncrement;
 }
 int Die::getIncrement(){
-
+    return increment;
 }
-void Die::setStartingValue(int newStartingSide){
-
+void Die::setStartingValue(int firstStartingVal){
+    startingVal = firstStartingVal;
 }
 int Die::getStartingValue(){
-
+    return startingVal;
 }
 void Die::setValue(int newValue){
-
+    value = newValue;
 }
 int Die::getValue(){
-
+    return value;
 }
 void Die::setColor(std::string newColor){
-
+    color = newColor;
 }
 std::string Die::getColor(){
-
+    return color;
 }
 void Die::setIsSymbol(bool newIsSymbol){
-
+    isSymbols = newIsSymbol;
 }
 bool Die::isSymbol(){
-
+    return isSymbols;
 }
 void Die::setMaterial(std::string newMaterial){
-
+    material = newMaterial;
 }
 std::string Die::getMaterial(){
-
+    return material;
 }
